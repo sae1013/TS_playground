c9d79b95a5b9944e3f42216c0e6dc5d0bace5cde
sae1013 <sae1013@gmail.com>
2022-09-23 19:53:58 +0900

    데코레이터 DOM 조작 & 프로퍼티 데코레이터?

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,27 +1,52 @@
 //코드 평가과정에서 데코레이터를 최상단으로 끌어올린다.
-console.log('mainThread 시작')
+
 function Logger(logTitle:string) {
     console.log('LoggerFactory()')
 
-    return (constructor:Person) => {
+    return (constructor:Function) => {
         console.log(logTitle,constructor)
     }
 }
 
-function Logger2(logTitle:string) {
-    console.log('LoggerFactory2()')
+function DomControl(id:string) {
 
-    return (constructor:Person) => {
-        console.log(logTitle,constructor)
+    return (constructor:any) => {
+        const domEle = document.querySelector('#root');
+        const instance = new constructor();
+        if(domEle){
+            domEle.innerHTML = instance.name
+        }
     }
 }
 
 @Logger('LOG:')
-@Logger2('LOG2:')
+@DomControl('root')
 class Person {
-    name="jmw93";
+    name="Decorator로 생성한 텍스트 입니다.";
+    age = 16;
     constructor() {
-        console.log('constructor()');
+
     }
 }
-console.log('mainThred 끝')
+
+
+function Log(target:any,propertyName:string){
+    console.log('Property Decorator')
+    console.log(target,propertyName)
+}
+
+class Product {
+    @Log
+    title:string;
+    private _price:number;
+
+    constructor(title:string,price:number) {
+        this.title =title
+        this._price = price
+    }
+
+    set price(val:number) {
+        if(val>0) this._price = val;
+        throw new Error('invalid Price');
+    }
+}
