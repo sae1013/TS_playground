fc1b31a9ccfc41a43491a502d11d99eeb153041c
sae1013 <sae1013@gmail.com>
2022-09-23 19:01:27 +0900

    데코레이터 함수 실행시점 & 데코레이터 팩토리 실행시점

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,17 +1,27 @@
-
+//코드 평가과정에서 데코레이터를 최상단으로 끌어올린다.
+console.log('mainThread 시작')
 function Logger(logTitle:string) {
     console.log('LoggerFactory()')
 
     return (constructor:Person) => {
-        console.log(logTitle,)
+        console.log(logTitle,constructor)
+    }
+}
+
+function Logger2(logTitle:string) {
+    console.log('LoggerFactory2()')
+
+    return (constructor:Person) => {
         console.log(logTitle,constructor)
     }
 }
 
 @Logger('LOG:')
+@Logger2('LOG2:')
 class Person {
     name="jmw93";
     constructor() {
         console.log('constructor()');
     }
 }
+console.log('mainThred 끝')
